23fb58916543e5b7cef051f0619817bea02a1626
Yinyao <755001538@qq.com>
2021-06-06 22:44:56 +0800

    master

---
+++ src/go_code/project02/main/heart.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("     **\t    **\t\n",
+		"  *     *     *\n",
+		"*      andi     *\n",
+		"   *         *\n",
+		"      *   *\n",
+		"        *")
+
+}
